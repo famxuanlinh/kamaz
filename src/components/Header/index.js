974209c3038f54730e514511ecbb791179bfc8d1
FamManh <fammanh217@mail.ru>
2022-08-31 23:08:07 +0700

    Base project

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Header = () => {
-    return <div>Header qq</div>;
+    return <header className={cx('wrapper')}>Header qq</header>;
 };
 
 export default Header;
