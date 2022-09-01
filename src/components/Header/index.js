7679f4514839ea1d2fa230dadc5b580508446424
FamManh <fammanh217@mail.ru>
2022-09-01 10:32:32 +0700

    add bootstrap

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { Button } from 'reactstrap';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
-    return <header className={cx('wrapper')}>Header qq</header>;
+    return (
+        <header className={cx('wrapper')}>
+            <Button color="danger">Danger!</Button>
+        </header>
+    );
 };
 
 export default Header;
