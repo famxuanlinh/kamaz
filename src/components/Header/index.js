4f1ffff8adccd76d4fa9ab9c4603bcd15e1067f6
famxuanlinh <famxuanlinh@gmail.com>
2022-09-09 17:19:45 +0700

    header 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Announcement from './Announcement';
+import Navbar from './Navbar';
 
 const Header = () => {
-    return <Announcement />;
+    return (
+        <div>
+            <Announcement />
+            <Navbar />
+        </div>
+    );
 };
 
 export default Header;
